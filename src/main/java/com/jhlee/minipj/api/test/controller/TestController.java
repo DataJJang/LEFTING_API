9bf9cc402286c3ae510ad8763309eb9c52105977
janghoonLee <jhlee@openit.co.kr>
2019-05-06 16:52:04 +0900

    mapper change

--- src/main/java/com/jhlee/minipj/api/test/controller/TestController.java
+++ src/main/java/com/jhlee/minipj/api/test/controller/TestController.java
@@ -2,14 +2,18 @@ package com.jhlee.minipj.api.test.controller;
 
 import com.jhlee.minipj.api.common.abstracts.AbstractController;
 import com.jhlee.minipj.api.common.base.model.ResultVO;
+import com.jhlee.minipj.api.common.constant.MessageConstants;
+import com.jhlee.minipj.api.common.exceptions.ParameterInvalidException;
 import com.jhlee.minipj.api.test.model.TestVO;
 import com.jhlee.minipj.api.test.service.TestService;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,12 +27,17 @@ public class TestController  extends AbstractController {
   TestService testService;
 
   @PostMapping(value = "/test", consumes = "application/json")
-  public @ResponseBody
-  ResultVO test1(TestVO testVO) throws Exception {
+  public @ResponseBody ResultVO test1(@RequestBody TestVO testVO) throws Exception {
 
+//    if (StringUtils.isEmpty(testVO.getMemId())) {
+//      throw new ParameterInvalidException(MessageConstants.ResponseEnum.PARAM_REQUIRED.getCode());
+//    }
     ResultVO resultVO = new ResultVO();
     Map<String, Object> pMap = testService.testInfo(testVO);
-
+    if (pMap.get("code") != null && pMap.get("msg") != null) {
+      resultVO.setCode(String.valueOf(pMap.get("code")));
+      resultVO.setMessage(String.valueOf(pMap.get("msg")));
+    }
     //service 연계
 
     return resultVO;
